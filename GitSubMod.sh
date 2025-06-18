36128c2d3984f30bf3c07f62618acfb0ac3762d3
Tsinswreng <Tsinswreng@gmail.com>
2025-06-18 10:09:01 +0800

    改子模塊

--- GitSubMod.sh
+++ GitSubMod.sh
@@ -2,9 +2,11 @@ git submodule add  https://github.com/Tsinswreng/Ngaq.Core.git  Ngaq.Core
 git submodule add  https://github.com/Tsinswreng/Ngaq.Local.git  Ngaq.Local
 git submodule add  https://github.com/Tsinswreng/Ngaq.Frontend.git  Ngaq.Frontend
 git submodule add  https://github.com/Tsinswreng/Ngaq.Server.git  Ngaq.Server
+git submodule add  https://github.com/Tsinswreng/Ngaq.Test.git  Ngaq.Test
 
 git submodule add  https://github.com/Tsinswreng/CsTypeAlias.git  Tsinswreng.CsTypeAlias
 git submodule add  https://github.com/Tsinswreng/CsCore.git  Tsinswreng.CsCore
 git submodule add  https://github.com/Tsinswreng/CsDictMapper.git  Tsinswreng.CsDictMapper
 git submodule add  https://github.com/Tsinswreng/CsSqlHelper.git  Tsinswreng.CsSqlHelper
+git submodule add  https://github.com/Tsinswreng/CsUlid.git  Tsinswreng.CsUlid
 
