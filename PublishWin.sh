057d236602f737cc818bcc39b53dfcd9ff8dd2ae
Tsinswreng <Tsinswreng@gmail.com>
2025-11-08 23:42:56 +0800

    初發版

--- PublishWin.sh
+++ PublishWin.sh
@@ -1,2 +1,7 @@
 cd Ngaq.Frontend/proj/Ngaq.Windows
 dotnet publish -c Release -r win-x64
+cd ./bin/Release/net9.0/win-x64
+cp -r publish publishNoPdb
+cd publishNoPdb
+rm -r *.pdb
+tar -czf Ngaq.Windows.tar.gz .
