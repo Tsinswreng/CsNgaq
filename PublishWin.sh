ae415abe9f1abe7d46f8edf1a308b27ee7f967da
Tsinswreng <Tsinswreng@gmail.com>
2025-11-10 23:03:49 +0800

    發版 2025_1110_225024

--- PublishWin.sh
+++ PublishWin.sh
@@ -1,7 +1,22 @@
+# TODO 打包語言翻譯json文件
+#CpAssets="$PWD/CpAssets.sh"
 cd Ngaq.Frontend/proj/Ngaq.Windows
+mkdir -p publish
+mv publish publishOld
 dotnet publish -c Release -r win-x64
+rm -r publishOld
+
+#sh $CpAssets
+cd ../../../
+sh ./CpAssets.sh
+cd Ngaq.Frontend/proj/Ngaq.Windows
+
 cd ./bin/Release/net9.0/win-x64
+mkdir -p publishNoPdb
+rm -r publishNoPdb
 cp -r publish publishNoPdb
 cd publishNoPdb
 rm -r *.pdb
-tar -czf Ngaq.Windows.tar.gz .
+tar -czf ../Ngaq.Windows.tar.gz .
+cd ..
+mv Ngaq.Windows.tar.gz publishNoPdb/
